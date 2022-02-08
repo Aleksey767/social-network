63a45ade51bf45904b1f9199087adf102ec285ec
Aleksey767 <98593351+Aleksey767@users.noreply.github.com>
2022-02-08 23:42:28 +0300

    64 add button disable

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "1f218eda-878d-4977-8376-374ae9d7c8ef"
     }
 });
 
