41c77b1a2882e771ad64c135416fc26d45d36a72
Aleksey767 <98593351+Aleksey767@users.noreply.github.com>
2022-02-09 23:26:32 +0300

    67 add redux-thunk

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,5 @@
+import {usersAPI} from "../api/api";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
@@ -8,13 +10,13 @@ let initialState = {
         {id: 2, message: 'It\'s my first post', likesCount: 11},
         {id: 3, message: 'Blabla', likesCount: 11},
         {id: 4, message: 'Dada', likesCount: 11}
-    ], 
+    ],
     profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
 
-    switch(action.type) {
+    switch (action.type) {
         case ADD_POST: {
             let newPost = {
                 id: 5,
@@ -43,8 +45,13 @@ const profileReducer = (state = initialState, action) => {
 
 
 export const addPostActionCreator = () => ({type: ADD_POST})
-export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE,profile })
+export const getUserProfile = (userId) => (dispatch) => {
+            usersAPI.getProfile(userId).then(response=>{
+                dispatch(setUserProfile(response.data))
+            })
+}
 export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text })
+    ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export default profileReducer;
