dab6bc4cc5a5ba98a6e1d334d817d6ffb373dcf6
Aleksey767 <98593351+Aleksey767@users.noreply.github.com>
2022-02-13 14:01:35 +0300

    fix redux-form

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
+import {Field, reduxForm} from "redux-form";
+const AddNewPostsForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={"textarea"} name={"newPostText"} placeholder='Your post' className={s.input}
+                />
+            </div>
+            <div>
+                <button className={s.btn}>Add post</button>
+            </div>
+        </form>
+    )
+}
+const AddNewPostForm = reduxForm({form:'ProfileAddNewPostsForm'})(AddNewPostsForm)
 const MyPosts = (props) => {
     let postsElements =
-        props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
+        props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
     let newPostElement = React.createRef();
 
-    let onAddPost = () => {
-        props.addPost();
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
     }
 
-    let onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
-    }
 
     return (
         <div className={s.postsBlock}>
             <h1>My posts</h1>
-            <div>
-                <div>
-                    <textarea placeholder='Your post' className={s.input} onChange={ onPostChange } ref={newPostElement}
-                              value={props.newPostText} />
-                </div>
-                <div>
-                    <button className={s.btn} onClick={ onAddPost }>Add post</button>
-                </div>
-            </div>
+            <AddNewPostForm onSubmit={onAddPost}/>
             <div className={s.posts}>
-                <span>{ postsElements }</span>
+                <span>{postsElements}</span>
             </div>
         </div>
     )
 }
+
+
 
 export default MyPosts;
