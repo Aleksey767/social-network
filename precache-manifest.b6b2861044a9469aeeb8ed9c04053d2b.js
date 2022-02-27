a0ef82a21dd2f094abbc0aeed22a584e31be6c21
Aleksey767 <al_sazonov@list.ru>
2022-02-27 14:40:51 +0300

    Updates

---
+++ precache-manifest.b6b2861044a9469aeeb8ed9c04053d2b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "01961a28ef08209f13f6",
+    "url": "/social-network/static/css/main.5ed67098.chunk.css"
+  },
+  {
+    "revision": "01961a28ef08209f13f6",
+    "url": "/social-network/static/js/main.01961a28.chunk.js"
+  },
+  {
+    "revision": "9ff2b6fcbda1d994a39e",
+    "url": "/social-network/static/css/1.a061ed16.chunk.css"
+  },
+  {
+    "revision": "9ff2b6fcbda1d994a39e",
+    "url": "/social-network/static/js/1.9ff2b6fc.chunk.js"
+  },
+  {
+    "revision": "9cd4949c6ea0f93e3534",
+    "url": "/social-network/static/css/2.d3eb0f2f.chunk.css"
+  },
+  {
+    "revision": "9cd4949c6ea0f93e3534",
+    "url": "/social-network/static/js/2.9cd4949c.chunk.js"
+  },
+  {
+    "revision": "21e819ebe71f1997b22a",
+    "url": "/social-network/static/js/3.21e819eb.chunk.js"
+  },
+  {
+    "revision": "e2ba233b6f69e9625400",
+    "url": "/social-network/static/js/runtime~main.e2ba233b.js"
+  },
+  {
+    "revision": "e27c8735da98ec6ccdcf12e258b26475",
+    "url": "/social-network/static/media/user.e27c8735.png"
+  },
+  {
+    "revision": "c742546a230fc2a888fcad3167752a62",
+    "url": "/social-network/static/media/logo.c742546a.png"
+  },
+  {
+    "revision": "d040818b222c91ea4191741808fb5400",
+    "url": "/social-network/index.html"
+  }
+];
